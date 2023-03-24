9cb381251e89db235e7e4bcbf1cacc66731cbb3b
dias19 <dias.nurmukhambetov@gmail.com>
2023-03-24 14:55:47 +0600

    first commit

---
+++ components/header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import style from "~/styles/Header.module.css";
+
+export default function Header() {
+  return (
+    <header className={style.container}>
+      <div className={style.logo}>logo</div>
+      <div className={style.navbar}>
+        <ul className={style.about}>About</ul>
+        <ul className={style.projects}>Projects</ul>
+        <ul className={style.experience}>Experience</ul>
+        <button className={style.button}>Resume</button>
+      </div>
+    </header>
+  );
+}
